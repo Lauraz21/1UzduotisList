43ac0095ddeec7907a4f74be0fbf76a998cade61
Laura <Laura@DESKTOP-BESSB6E>
2023-09-25 20:22:42 +0300

    Add project files.

---
+++ 1UzduotisList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace _1UzduotisList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> city = new List<string>();
+
+            city.Add("Vilnius");
+            city.Add("Kaunas");
+            city.Add("Klaipeda");
+
+            LengthOfCity(city);
+        }
+        public static void LengthOfCity(List<string> city)
+        {
+            foreach (string s in city)
+            {
+                Console.WriteLine(s.Length);
+            }
+        }
+    }
+}
